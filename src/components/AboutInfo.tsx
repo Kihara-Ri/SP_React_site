79e10188b3d3300053cb640dffedca1929f332a9
kihara <limuyuan3650@foxmail.com>
2024-12-28 16:32:33 +0000

    修复问题: 调整图标引用硬编码

--- src/components/AboutInfo.tsx
+++ src/components/AboutInfo.tsx
@@ -8,14 +8,14 @@ const AboutInfo: React.FC = () => {
       <p>我们是一个拥有200+成员的Minecraft玩家群体</p>
       <p>无论你是</p>
       <p>
-        <img src='/public/mc_icons/redstone_dust_icon.png' className="inline-icon" />
+        <img src='/mc_icons/redstone_dust_icon.png' className="inline-icon" />
         硬核生电玩家, 还是
-        <img src="/public/mc_icons/brick_icon.png" className="inline-icon" />
+        <img src="/mc_icons/brick_icon.png" className="inline-icon" />
         建筑玩家</p>
       <p>
-        <img src='/public/mc_icons/fishing_rod_icon.png' className="inline-icon" />
+        <img src='/mc_icons/fishing_rod_icon.png' className="inline-icon" />
         养老党, 或是
-        <img src='/public/mc_icons/pickaxe_icon.png' className="inline-icon" />
+        <img src='/mc_icons/pickaxe_icon.png' className="inline-icon" />
         新手</p>
       <p>都能找到志同道合的伙伴</p>
       <p>或是只想在群里吹水</p>
